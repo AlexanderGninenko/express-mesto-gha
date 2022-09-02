fc188b3d248845a148bfb09115f34b5a876e50c9
AlexanderGninenko <alexander.gninenko@icloud.com>
2022-09-02 17:12:25 +1000

    upd

--- app.js
+++ app.js
@@ -18,32 +18,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const app = express();
 
-const allowedCors = [
-  'https://alexandergninenko.nomoredomains.sbs',
-  'https://api.alexandergninenko.nomoredomains.sbs',
-  'localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    return res.status(200).send();
-  }
-
-  next();
-});
-
-// app.use('*', cors(corsOptions));
+app.use('*', cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());

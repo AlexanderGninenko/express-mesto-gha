bd9335a005dd6cc0b9cb5a2ee4ad7cf1b8cf3a33
AlexanderGninenko <alexander.gninenko@icloud.com>
2022-09-05 19:32:00 +1000

    upd

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -33,4 +32,4 @@ app.use(errors());
 
 app.use(errorUniHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT);
